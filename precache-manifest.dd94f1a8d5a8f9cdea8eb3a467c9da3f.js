f6d5266d88e5bfe90c749fe8557059b576c9525e
Hummara Bashir <bashirh@state.gov>
2024-08-22 10:23:34 -0400

    Updates

---
+++ precache-manifest.dd94f1a8d5a8f9cdea8eb3a467c9da3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "831b30727b7039de5a28",
+    "url": "/plannerboard-react-app/static/css/main.28c2d76c.chunk.css"
+  },
+  {
+    "revision": "831b30727b7039de5a28",
+    "url": "/plannerboard-react-app/static/js/main.831b3072.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/plannerboard-react-app/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "486df3441def4968f75a",
+    "url": "/plannerboard-react-app/static/js/2.486df344.chunk.js"
+  },
+  {
+    "revision": "e5fba7ac34564ac27df1",
+    "url": "/plannerboard-react-app/static/js/runtime~main.e5fba7ac.js"
+  },
+  {
+    "revision": "77ce2909747176f4486d56e018f41868",
+    "url": "/plannerboard-react-app/static/media/bg.77ce2909.jpg"
+  },
+  {
+    "revision": "2897dbbbebfc196d4ba5389a4d26b266",
+    "url": "/plannerboard-react-app/index.html"
+  }
+];
